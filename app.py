f4ad49f0b3757dbb4e402bbe45475d92703b4d88
YASİN ÖZTÜRK <yasinozturk@192.168.1.10>
2026-09-04 11:32:21 +0300

    Add Dockerfile

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ def home():
     return "DevSecOps güvenli web uygulaması"
                                                    
 if __name__== "__main__":
-     app.run(debug=True)
+     app.run(host="0.0.0.0", port=5000)
 
